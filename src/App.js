ba224d3952cbfefae9a64529f00cbb46bfc8db66
unknown <furkanyilmazx@gmail.com>
2019-01-21 00:07:15 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { DatePicker } from 'antd';
+import logo from './logo.svg';
+import SiderDemo from './layout.js'
+import 'antd/dist/antd.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <SiderDemo/>
+    );
+  }
+}
+
+export default App;
